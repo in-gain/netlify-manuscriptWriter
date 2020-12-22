628942d8c68ca52f762cecf763f744f0626c9849
in-gain <online_game_kudryavka@yahoo.co.jp>
2020-12-22 21:56:35 +0900

    add date

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <p>あなたは {{ date }} にこのサイトを訪れました。</p>
   </div>
 </template>
 
@@ -34,7 +35,13 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    date: Date
+  },
+  methods:{
+    window:onload = ()=>{
+      this.date = new Date();
+    }
   }
 }
 </script>
